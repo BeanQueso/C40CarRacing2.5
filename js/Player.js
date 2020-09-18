e987d19553c9231662eac7141c2dc9a122421a99
eshaant7 <eshaant7@gmail.com>
2020-09-18 11:57:07 +0200

    Rank

--- js/Player.js
+++ js/Player.js
@@ -3,8 +3,9 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
-
+  
   getCount(){
     var playerCountRef = database.ref('playerCount');
     playerCountRef.on("value",(data)=>{
@@ -30,6 +31,17 @@ class Player {
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
+    })
+  }
+  getCarsAtEnd(){
+  var getCarsAtEndRef = database.ref('carsAtEnd'); 
+  getCarsAtEndRef.on("value",(data) =>{
+    this.rank = data.val();
+  })   
+  }
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carsAtEnd:rank,
     })
   }
 }
